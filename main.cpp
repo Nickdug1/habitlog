1155522317517467d033f7e56ff2d458c2c2a826
nickdug <nickdellwork@gmail.com>
2024-10-27 02:40:50 -0300

    Testin rebasing and merging conflicts locally

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "parsingfunctions.h"
+
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, GitHub!" << std::endl;
-    return 0;
 
+<<<<<<< HEAD
     // pull request test
     //OTÁRIO
+=======
+    //Check if log exists, if not create it
+    ofstream create_file("habitlog.txt", ios::app);
+    cout << "---------------------------------- habitlog ----------------------------------" << endl;
+    create_file.close();
+
+    read_tsplog("habitlog.txt");
+
+    return 0;
+>>>>>>> ad62a0d (Basic parsing function for tsp files (text_string_spaced files) and new header)
 }
---
+++ parsingfunctions.h
@@ -0,0 +1,6 @@
+#ifndef PARSINGFUNCTIONS_H
+#define PARSINGFUNCTIONS_H
+
+std::vector<std::vector<std::string>> read_tsplog(std::string);
+
+#endif
