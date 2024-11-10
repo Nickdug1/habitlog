0bcfbcee26ee5723850e071f315a5d5cc242a2e9
nickdug <nickdellwork@gmail.com>
2024-11-10 02:33:46 -0300

    New UI functions: selecthabit(), edithabit() [not finished], choose_functionality()

--- UIfunctions.cpp
+++ UIfunctions.cpp
@@ -17,7 +17,7 @@ vector<string> create_new_habit() {
 
     //descrição
     string descricao;
-    cout << "Qual a descrição do hábito?" << endl << endl;
+    cout << "Qual a descrição do hábito?" << endl;
     getline(cin, descricao);
     cout << endl;
     //Parser " " to "_" (criar função em parsingfunctions)
@@ -46,5 +46,87 @@ vector<string> create_new_habit() {
 	//cria o novo habito e coloca no log(vetor)
     vector<string> novo_habito = {nome, descricao, codigo_dia, "0", "0", "0"};
     return novo_habito;
+}
+
+int select_habit(vector<vector<string>> habitlog){
+    string name;
+    cout << "Nome do hábito: ";
+    cin >> name;
+    cout << endl;
+    
+    int habitnumber;
+    for (int i = 0; i < habitlog.size(); i++){
+        if (habitlog[i][0] == name){
+        	habitnumber = i;
+            break;
+        } 
+    }
+    return habitnumber;
+}
+
+
+void edit_habit(int habitnumber, vector<vector<string>>& habitlog){
+    int back = 0;
+    vector<string> funcvec = {"ChangeName", "ChangeDesc", "ChangeDays", 
+    	    "ResetStreak", "Remove"};
+    while (back == 0){ 
+	    char func = choose_func(funcvec, "Back", 'b');
+	    
+	    switch(func){
+	    	case '0':{
+	    	    string newname;
+				cout << "New name: ";
+				cin >> newname;
+				cout << endl;
+				habitlog[habitnumber][0] = newname;
+            }
+	    	break;
+
+	    	case '1':{
+				string newdesc;
+				cout << "New description: ";
+				cin >> newdesc;
+				cout << endl;
+	 			habitlog[habitnumber][1] = newdesc;
+			}	
+	    	break;
+
+	    	case '2':{
+				string new_days;
+			}
+	    	break;
+
+	    	case '3':
+
+	    	break;
+
+	    	case '4':
+
+	    	break;
+	    	
+	    	case 'b':
+				back = 1;
+	        break;
+
+	        default:
+	        cout << "Not implemented yet :P";
+	        break;
+	    }
+	}
+}
+
+char choose_func(vector<string> funcs, string exit, char exitchar){
+	char func;
+    int i = 0;
+
+    cout << "Choose a functionality [";
+	for (string& funcname : funcs){
+	cout << funcname << "(" << i << ") | ";
+    i++;
+    }
+    cout << exit << "(" << exitchar << ")]" << endl;
+
+    cin >> func;
+    return func;
 }
 
