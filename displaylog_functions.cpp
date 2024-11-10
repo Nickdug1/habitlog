ed7002b125574ae375892b6de8b84504126f4e3c
nickdug <nickdellwork@gmail.com>
2024-11-10 02:34:27 -0300

    New display_habit() function [requires more testing]

--- displaylog_functions.cpp
+++ displaylog_functions.cpp
@@ -1,5 +1,3 @@
-//void selevoid select_habit(string habit_name)
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,6 +13,7 @@ string center(string text, int width) {
 } 
 
 void display_log(vector<vector<string>> log) {
+    cout << "\033[2J\033[1;1H";
     const int HEADER_PADDING = 69;
     const int COLUMN_WIDTH = 15;
     const int TABLE_PADDING = (HEADER_PADDING*2 + 8 - 4*COLUMN_WIDTH)/2;
@@ -41,5 +40,44 @@ void display_log(vector<vector<string>> log) {
     cout << endl <<string(HEADER_PADDING*2 + 8 + 2, '-');
 }
 
+void display_habit(vector<vector<string>> log, int habitnumber){
+	cout << "\033[2J\033[1;1H";
+    const int HEADER_PADDING = 69;
+    const int COLUMN_WIDTH = 15;
+    const int TABLE_PADDING = (HEADER_PADDING*2 + 8 - 7*COLUMN_WIDTH)/2;
 
+    int habitsize = log[habitnumber].size();
+	cout << log[habitnumber][0] << endl << endl;
+
+    cout << string(HEADER_PADDING*2 + 8 + 2, '-');
+
+	cout << "Reps: " << log[habitnumber][habitsize - 3] << endl;
+	cout << "Streak: " << log[habitnumber][habitsize - 2] << endl;
+	cout << "MaxStreak: " << log[habitnumber][habitsize - 1] << endl;
+
+    cout << string(HEADER_PADDING*2 + 8 + 2, '-');
+
+    cout << string(TABLE_PADDING, ' ');
+    cout << center("Segunda-feira", COLUMN_WIDTH) << "|";
+	cout << center("Terça-feira", COLUMN_WIDTH) << "|";
+	cout << center("Quarta-feira", COLUMN_WIDTH) << "|";
+	cout << center("Quinta-feira", COLUMN_WIDTH) << "|";
+	cout << center("Sexta-feira", COLUMN_WIDTH) << "|";
+	cout << center("Sábado", COLUMN_WIDTH) << "|";
+	cout << center("Domingo", COLUMN_WIDTH) << "|" << endl;
+	cout << string(TABLE_PADDING, ' ');
+	for (char& binary : log[habitnumber][2]){
+		if (binary == 1){
+			cout << center("x", COLUMN_WIDTH) << "|";
+		}
+		else{
+			cout << center(" ", COLUMN_WIDTH) << "|";
+		}
+	}
+	cout << endl;
+
+    cout << string(HEADER_PADDING*2 + 8 + 2, '-');
+
+    cout << "Descrição: " << endl << "    " << log[habitnumber][1] << endl;
+}
 
