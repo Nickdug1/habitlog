5851a414784e5ad8171d87e840900ca0b724c2e3
nickdug <nickdellwork@gmail.com>
2024-11-10 02:38:23 -0300

    Main Loop and some switch [Some functions are creating new vectors instead of constant reference, this should be corrected]

--- functions.h
+++ functions.h
@@ -10,5 +10,9 @@ void oldchar_newchar(std::string& word, char oldchar, char newchar);
 void display_log(std::vector<std::vector<std::string>> log);
 void select_habit(std::string habit_name);
 std::vector<std::string> create_new_habit();
+char choose_func(std::vector<std::string> funcs, std::string exit, char exitchar);
+void edit_habit(int habitnumber, std::vector<std::string>& habitlog);
+
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -18,15 +18,24 @@ int main() {
 
     vector<vector<string>> habitlog = read_tsplog("habitlog.txt"); // habito = <habitname desc 1001110 rep streak maxstreak>  
 
+    int exit = 0;
+    while (exit == 0){
     display_log(habitlog);
 
     //ask for functionality [SelectHabit | UpdateDailyLog | CreateNewHabit | ShowRecords]
-        //select_habit() will display habit and give options: [Edit | Back]
-            //edit_habit() will give options: [ChangeName | ChangeDesc | ChangeAtributtes | ResetStreak | Remove]
-                //change_atributtes() will give options: [ChangeName | ChangeAttribute | Reset | Remove], Atributos serão 
+    vector<string> funcvec = {"SelectHabit", "UpdateDailyLog", "CreateNewHabit", "ShowRecords"};
+    char func = choose_func(funcvec, "Exit", 'e');
+    switch(func){
+    	case '0':
+        //select_habit() will display habit description and give options: [Edit | Back]
+        int habitnunmber = select_habit(habitlog);
+        display_habit(habitlog, habitnumber);
+            //edit_habit() will give options: [ChangeName | ChangeDesc | ChangeDays | ResetStreak | Remove]
+                //change_atributtes() will give options: [ChangeAttribute | Reset | Remove], Atributos serão 
                 //log[i][2 + 2j], log[i][3 + 2j], log[i][4 + 2j] = attributej_nome, attribute type, attributej
                     //attribute types may be: additive, fixnumber, feeling
-                    
+        break;
+        //Case "1":            
         //update_daily_log() will prompt the user
             //Did you do 'current_habit' today? [Y/N]
             //Calculate streak and maxstreak
@@ -35,15 +44,25 @@ int main() {
                 //else go for next attribute until habit_lenght - 4
                 //display habit stats again
                 //prompt the user to redo or leave [redo | leave]
-
-        //createnewhabit()
+        //break;
+        case '2':
+        //createnewhabit
         habitlog.push_back(create_new_habit());
         write_tsplog(habitlog, "habitlog.txt");
-        
+        break; 
+        //Case "3":
         //showrecords() prompt the user [Monthly | Annual | All | Leave]
              //display and prompt user again 
-
+        //break;
+        case 'e':
+            exit = 1;
+        break;
+        
+        default:
+        cout << "Funcionalidade não implementada ainda :P";
+    }
         //put stuff in the log
+    }
         
     return 0;
 }
